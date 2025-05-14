b81dce9162419c7905fb6a7c118dd4160a95ea59
Sophia Custódio <Solguedesdaluz@gmail.com>
2025-05-14 20:17:56 -0300

    Add files via upload

--- Atividade 4.py
+++ Atividade 4.py
@@ -6,11 +6,14 @@ def main ():
     av1 = float(input("Digite sua nota na primeira avaliação: "))
     av2 = float(input("Digite sua nota na segunda avaliação: "))
     media = (av1 + av2)/2
+    situacao = "APROVADO" if media >=6 else "REPROVADO"
     
-    alunos.append([nome, av1, av2, media])
+    alunos.append([nome, av1, av2, media, situacao])
     print("\n---Dados dos Alunos---")
     for aluno in alunos:
-        print(f"NOME: {aluno[0]} | AV1:{aluno[1]:.2f} | AV2:{aluno[2]:.2f} | Média: {aluno[3]:.2f}")
+        print(f"NOME: {aluno[0]} | AV1:{aluno[1]:.2f} | AV2:{aluno[2]:.2f} | Média: {aluno[3]:.2f} | Situação: {aluno[4]}")
+    
+       
 
 if __name__=="__main__":
    main()
